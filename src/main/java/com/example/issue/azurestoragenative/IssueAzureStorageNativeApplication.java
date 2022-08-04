40e0d30248d33b3686b7aad5761d545cab927762
Shili Chen <v-shilichen@microsoft.com>
2022-08-04 12:38:52 +0800

    initial the issue code

---
+++ src/main/java/com/example/issue/azurestoragenative/IssueAzureStorageNativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.issue.azurestoragenative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueAzureStorageNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueAzureStorageNativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/issue/azurestoragenative/IssueAzureStorageNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.issue.azurestoragenative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueAzureStorageNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
