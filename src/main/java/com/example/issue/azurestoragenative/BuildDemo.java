18c28ce0046fce669a14a1b1229e4ded355bdbb5
Shili Chen <v-shilichen@microsoft.com>
2022-08-22 20:24:43 +0800

    Add test code without Spring Native

---
+++ src/main/java/com/example/issue/azurestoragenative/BuildDemo.java
@@ -0,0 +1,9 @@
+package com.example.issue.azurestoragenative;
+
+import com.azure.spring.cloud.autoconfigure.compatibility.AzureCompatibilityVerifierAutoConfiguration;
+
+public class BuildDemo {
+    public static void main(String[] args) {
+        System.out.println(new AzureCompatibilityVerifierAutoConfiguration());
+    }
+}
